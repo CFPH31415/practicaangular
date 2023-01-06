46909b55d1956ef900161f82c28763c726a1a3c6
Carlos Portillo <carlosportillo308@gmail.com>
2023-01-05 22:15:52 -0500

    logica agregada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LateralComponent } from './components/lateral/lateral.component';
 import { CentroSuperiorComponent } from './components/centro-superior/centro-superior.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CentroInferiorComponent } from './components/centro-inferior/centro-inferior.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LateralComponent,
-    CentroSuperiorComponent,
-    CentroInferiorComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LateralComponent,
+        CentroSuperiorComponent,
+        CentroInferiorComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+    ]
 })
 export class AppModule { }
--- src/app/components/centro-inferior/centro-inferior.component.ts
+++ src/app/components/centro-inferior/centro-inferior.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-centro-inferior',
   templateUrl: './centro-inferior.component.html',
   styleUrls: ['./centro-inferior.component.css']
 })
 export class CentroInferiorComponent {
+  userForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  //Dando validaciones
+
+  ngOnInit() {
+    this.userForm = this.fb.group({
+      firstName: [Validators.required, Validators.maxLength(10)],
+      lastName: [Validators.required, Validators.maxLength(10)],
+      email: [Validators.required, Validators.email],
+      message: [Validators.required, Validators.maxLength(100)],
+   
+    });
+
+    this.loadApi()
+  }
+
+  //Valores predeterminados con los que se cargará el formulario
+
+  loadApi():any{
+    const response = {
+      firstName: "Carlos",
+      lastName: "Portillo",
+      email: "carlosportillo308@gmail.com",
+      message: "Escribe tu mensaje"
+    }
+
+    this.userForm.patchValue(response);
+
+  }
+
+  submitForm() {
+    console.log('Formulario enviado con el valor: ', this.userForm.value);
+  }
 }
